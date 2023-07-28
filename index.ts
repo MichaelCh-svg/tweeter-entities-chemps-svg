bab036ad0056403250bfcf6df14ba465fb4a6c52
MichaelCh-svg <champeniosmichael@gmail.com>
2023-07-28 13:50:05 -0600

    export explicitly so react can import from index

--- index.ts
+++ index.ts
@@ -4,5 +4,8 @@ export { PostSegment, Type } from "./lib/domain/PostSegment";
 export { Status } from "./lib/domain/Status";
 export { User } from "./lib/domain/User";
 export { AuthToken } from "./lib/domain/AuthToken";
-export * from "./lib/net/Request";
-export * from "./lib/net/Response";
+
+// export * does not work when loading this in the client. 
+// Instead we have to list each export.
+export { LoginRequest, RegisterRequest, AuthorizedRequest, GetUserRequest, OtherUserRequest, PostStatusRequest, FollowListRequest, StoryFeedRequest } from "./lib/net/Request";
+export { Response, AuthenticateResponse, FollowCountResponse, UserResponse, IsFollowingResponse, FollowListResponse, StoryFeedResponse }  from "./lib/net/Response";
